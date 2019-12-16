e5ae170fe80aeb78aa3db718cadd83ffa27e352a
Panagiotis Prattis <panagiotispr1997@gmail.com>
2019-12-16 15:41:10 +0200

    Update knn_ratings_float.java

--- knn_ratings/src/knn_ratings/knn_ratings_float.java
+++ knn_ratings/src/knn_ratings/knn_ratings_float.java
@@ -11,9 +11,7 @@ public class knn_ratings_float {
 	public static int[][] ratings = new int[20][6];
 	
 	public static void main(String[] args){
-		int[][] ratings = {
-				//{2,5,1,2,4,2,5,1,5,4,3,1,2,3,3,1,2,1,1,1,5,2,2,3,3,3,5,1,4,4,3,4,4,1,3,5,4,5,3,1,3,1,5,1,5,4,2,1,2,5,3,5,1,2,1,5,2,3,5,1,1,4,5,5,1},
-                 
+		int[][] ratings = {//(11 user ratings 1-5 for some locations                
                 {5,3,1,5,1,1,1,3,5,2,3,4,3,3,4,2,2,4,4,1,2,2,5,1,5,4,1,2,3,4,4,5,5,4,5,4,2,1,2,4,4,2,3,5,4,1,2,3,3,5,3,2,4,3,3,3,5,5,2,1,3,2,5,2,5}, 
                 {5,2,2,3,5,5,1,3,2,5,1,5,4,1,5,5,4,4,2,5,5,5,3,3,1,4,2,4,3,1,4,2,4,5,2,4,2,1,4,1,4,2,2,1,3,5,4,5,3,2,2,4,3,3,3,5,1,2,5,3,2,1,2,1,4}, 
                 {2,2,2,2,2,5,3,4,4,1,4,2,5,2,1,4,2,2,3,1,1,4,5,2,5,4,4,2,2,1,1,1,4,4,2,3,2,4,2,2,4,3,4,3,5,5,5,5,5,2,1,1,5,3,2,1,2,5,5,3,1,5,3,5,4}, 
@@ -30,7 +28,7 @@ public class knn_ratings_float {
               };
 		
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter 0-9");
+		System.out.println("Enter 0-11"); //pick user 5 (zero ratings)
 		
 		
 		int num = scan.nextInt();// the user index that we will check rest users for similarity
@@ -46,50 +44,24 @@ public class knn_ratings_float {
 		ArrayList<Double> ssim = new ArrayList<Double>(); //sorted 
 		
 		for(int i=0; i<ratings.length; i++) {	//for all users
-			/*for(int j=0; j<ratings[0].length; j++) { //for eacch topothesia
-				if(i != num && ratings[num][j]!=0 && ratings[i][j]!=0) { //an o pros eksetasi user den einai o user pou kanoume recommend (i!=num) & gia ta diathesima 
-					//ratings pou ehoume apo ton user pou provlepoume alla kai tous ypoloipous users pou eksetazoume
-					//euclidean
-					double diff = 0;
-					diff = ratings[num][j] - ratings[i][j]; 
-					sumSquares += diff*diff; //to be eucledian
-					flag = true;
-				}
-				else if (i == num) //an o xristis pros eksetasi einai o user pou theloume provlepsi
-					flag = false;
-			}
-			if(flag) { //an kaname SUGKRISI me olous ektos apo ton xristi pou epizitoume PROVLEPSI
-				double d = Math.sqrt(sumSquares); //eucledian dist
-				double similarity = 1/d;
-				if (Double.isInfinite(similarity)) {
-					similarity = 1; //einai EXACT SAME
-				}
-				
-				System.out.println("user: "+num+" has similarity to user: "+ i+" of value "+similarity); //similarity me tous ypoloipous users
-				user.add(i);
-				sim.add(similarity);
-				ssim.add(similarity);
-			}*/
-			
-			//GIA RATING DATASET EUCL, COSINE
+			//HEURISTICS for similarity
 			//euclideanDist(ratings, num, i, user, sim, ssim);
 			cosineSimilarity(ratings,i,num,user,sim,ssim);
 			
 		}
 
-		Collections.sort(ssim, Collections.reverseOrder()); //apo to mikrotero sto megalutero
+	    Collections.sort(ssim, Collections.reverseOrder()); //from smaller to bigger
 	    //Collections.reverse(ssim); //descending
-	    //int k=3; //5-nn
 	    int remove = ssim.size() - k;
 	    for (int i =0; i<remove;i++) {
-	    	ssim.remove(ssim.size()-1); //afairw tosous wste na meinoun k
+	    	ssim.remove(ssim.size()-1); //we want the k nearest neighbours to remain
 	    }
 	    boolean once = true;
 	    for (int i=0; i<ssim.size(); i++) {
 	    	for (int j=0; j<sim.size(); j++) {
 	    		if(Math.floor(ssim.get(i)*10000) == Math.floor(sim.get(j)*10000) && once && !suser.contains(user.get(j))) { // x10000 gia na emfanistoun arketa dekadika psifia wste na ginei match
-	    			once = false; //an o vrethei xristis me to parapanw if na ikanopoieitai tote exei vrethei IDI MIA FORA
-	    			suser.add(user.get(j)); //prosthese ton similar user me max cosine similarity
+	    			once = false; //if the user found above is satisfied then he has been found already
+	    			suser.add(user.get(j)); //add the similar user with max cosine similarity
 	    		}
 	    	}
 	    	once = true; 
@@ -101,62 +73,38 @@ public class knn_ratings_float {
 	    System.out.println(suser);
 	    System.out.println(ssim);
 	    
-	    //double[] recommended = new double[ratings[0].length]; //vathmologies oles gia ton user
+	    //double[] recommended = new double[ratings[0].length];
 
     	ArrayList <Double> recommended = new ArrayList<Double>();
     	ArrayList <String> recommended_name = new ArrayList<String>();
     	double weightedSum = 0; 
-	    double similaritySum = 0; //osoi users einai pio konta tote ypologizetai 
+	   double similaritySum = 0; //osoi users einai pio konta tote ypologizetai 
 	    
-	    for(int i=0; i<ratings[0].length; i++) { //gia n topothesies
-	    	for(int j=0; j<ratings.length-1; j++) { //gia m xristes
+	    for(int i=0; i<ratings[0].length; i++) { //for n locations
+	    	for(int j=0; j<ratings.length-1; j++) { //for m users
 	    		//if(ssim.contains(sim.get(j))) {
-	    		if(suser.contains(user.get(j))) { // an o user einai apo tous neighbors
-	    			if(ratings[num][i] == 0) { //an to rating tou pros eksetasi user DEN YPARXEI AKOMI
-	    				weightedSum += ratings[j][i]*sim.get(j); //tote i provlepsi prokuptei apo to rating tou neighbor sti sugkekrimeni topothesia X similarity metaksu twn 2 xrhstwn 
+	    		if(suser.contains(user.get(j))) { // if user is one of the knn
+	    			if(ratings[num][i] == 0) { //if the rating we are considering has not been found already
+	    				weightedSum += ratings[j][i]*sim.get(j); //then the prediction results from the neighbour rating at the specific location times similarity between 2 users
 	    				//System.out.println(user.get(j)+","+sim.get(j));
-	    				similaritySum += sim.get(j); // athroisma similarity of all neighbors
+	    				similaritySum += sim.get(j); // sum of similarity of all neighbours
 	    			}
 	    		}
 	    		
 	    	}
-	    	if(weightedSum == 0) { // an den yparxei kanenas neighbor h ola ta ratings tou user pros provlepsi yparxoun
-	    		double d = ratings[num][i]; //dwse apla to rating p ehei idi dwsei
+	    	if(weightedSum == 0) { // if there is no neigbours or all the ratings of the selected user for recommendation already exist
+			double d = ratings[num][i]; //give the rating that already exist
 	    		recommended.add(d);
 	    		//recommended[i] = d;
 	    	}
-	    	else { //an den ehei dwsei ratings se kapoies topothesies
-		    	weightedSum /= similaritySum; // diairese to ginomeno tis problepsis me to athroisma twn distances apo similar users gia na pROKUPSEI TO RATING
+	    	else { //if he has zero rating
+		    	weightedSum /= similaritySum; // divide the product of the prediction by the sum of the similars user distances to obtain the RATING
 		    	recommended.add(weightedSum);
 		    	//recommended[i] = weightedSum;
 	    	}
 	    	weightedSum=0;
 	    	similaritySum=0;
-	    }
-	    
-	    
-	    /*int[] maxTopo = new int[65]; //top 10 
-	    double[] maxRat = new double[65];
-	    maxRat[0] = 0;
-	    int j=0;
-	    for (int i=0; i< recommended.size();i++)
-	    {
-	    	if (recommended.get(i) > maxRat[j]) { //sorting kanw edw
-	    		maxRat[j] = recommended.get(i);
-	    		maxTopo[j] = i;
-	    		j++;
-	    		
-	    	}
-	    }
-	    
-	    for (k=0;k<maxRat.length;k++) {
-	    	System.out.println("TOPOTHESIA: " + maxTopo[k]);
-		    System.out.println("RATING: " + maxRat[k]);
-	    }
-	    */
-	    //for(double d: recommended)
-	    	//System.out.println(d);
-	    
+	    }     
 	    
     }
 	
@@ -164,7 +112,7 @@ public class knn_ratings_float {
 	public static void euclideanDist(int [][] ratings,int num, int i, ArrayList<Integer> user,ArrayList<Double> sim,ArrayList<Double> ssim) {
 		double sumSquares = 0;
 		boolean flag = true;
-		for(int j=0; j<ratings[0].length; j++) { //for eacch topothesia
+		for(int j=0; j<ratings[0].length; j++) { //for each location
 			if(i != num && ratings[num][j]!=0 && ratings[i][j]!=0) { //an o pros eksetasi user den einai o user pou kanoume recommend (i!=num) & gia ta diathesima 
 					//ratings pou ehoume apo ton user pou provlepoume alla kai tous ypoloipous users pou eksetazoume
 					//euclidean
